b80d79706425fb43013cea661ccf114c11ef973d
Diego Aprigio <57737033+apriginh0@users.noreply.github.com>
2022-06-11 00:31:14 -0300

    atualizando projeto covid-19

--- src/containers/Main/Components/Board.jsx
+++ src/containers/Main/Components/Board.jsx
@@ -6,13 +6,25 @@ import Skeleton from "../../../components/Skeleton";
 import Card from "./Card";
 
 function Board({data}) {
-  const { cases, todayDaths, recover, deaths, todayCases } = data
+  const { cases, todayDaths, recovered, deaths, todayCases } = data
   const getValue = (value) => value ? value : <Skeleton variant="text" width={182} height={60} />
 
   return (
     <Grid container spacing={4}>
       <Grid item xs={12} md={3}>
         <Card value={getValue(cases)} label="Total de casos" color="#5d78ff" />
+      </Grid>
+      <Grid item xs={12} md={3}>
+        <Card value={getValue(todayCases)} label="Casos de hoje" color="#f8e007" />
+      </Grid>
+      <Grid item xs={12} md={3}>
+        <Card value={getValue(deaths)} label="Total de óbitos" color="#000" />
+      </Grid>
+      <Grid item xs={12} md={3}>
+        <Card value={getValue(todayDaths)} label="Óbitos de hoje" color="#f06969" />
+      </Grid>
+      <Grid item xs={12} md={3}>
+        <Card value={getValue(recovered)} label="Total de recuperados" color="#04e710" />
       </Grid>
     </Grid>
   )
--- src/containers/Main/index.jsx
+++ src/containers/Main/index.jsx
@@ -1,11 +1,13 @@
 import React, { memo, useState, useCallback, useEffect } from 'react'
 import api from '../../api'
 import Board from './Components/Board'
+import Panel from './Components/panel'
 import { ContainerStyled } from './Components/style'
 
 function Main() {
   const [data, setData] = useState({})
-const [country, setCountry] = useState('Brazil')
+  const [country, setCountry] = useState('Brazil')
+  const updateAt = new Date().toLocaleString()
 
   const getCovidData = useCallback((country) => {
     api.getCountry(country)
@@ -14,11 +16,16 @@ const [country, setCountry] = useState('Brazil')
   useEffect(() => {
     getCovidData(country)
   }, [getCovidData, country])
+
+  const handleChange = ({ target }) => {
+    const country = target.value
+    setCountry(country)
+  }
   
   return (
    <ContainerStyled>
      <div className="mb-2" >
-
+      <Panel data={data} updateAt={updateAt} onChange={handleChange} country={country} getCovidData={getCovidData} />
      </div>
      <Board data={data} />
    </ContainerStyled>
