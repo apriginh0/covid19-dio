d5cccb6c5330e1d83d56aec720453fa42a6136ce
Diego Aprigio <57737033+apriginh0@users.noreply.github.com>
2022-06-12 00:53:50 -0300

    correções estéticas2

--- src/containers/Main/Components/panel.jsx
+++ src/containers/Main/Components/panel.jsx
@@ -1,12 +1,14 @@
-import React, { memo } from "react";
-import RefreshIcon from "../../../assets/images/refresh.svg"
-import { Card, Typography, Button, Select, MenuItem } from "../../../components"
-import COUNTRIES from "../../../commons/constants/countries"
-import { CardPanelContentStyled, ItemStyled } from "./style";
+import React, { memo } from 'react'
+import RefreshIcon from '../../../assets/images/refresh.svg'
+import { Card, Typography, Button, Select, MenuItem } from '../../../components'
+import COUNTRIES from '../../../commons/constants/countries'
+import { CardPanelContentStyled, ItemStyled } from './style'
 
 const navigatorHasShare = navigator.share
-function Panel({updateAt, onChange, data, country, getCoviddata}) {
+
+function Panel({ updateAt, onChange, data, country, getCoviddata }) {
   const { cases, recovered, deaths, todayCases, todayDeaths } = data
+
   const renderCountries = (country, index) => (
     <MenuItem key={`country-${index}`} value={country.value}>
       <ItemStyled>
@@ -15,24 +17,29 @@ function Panel({updateAt, onChange, data, country, getCoviddata}) {
       </ItemStyled>
     </MenuItem>
   )
+
   const textCovid19 = `País: ${country} - recuperados: ${recovered}`
+
   const copyInfo = () => {
     navigator.clipboard.writeText(textCovid19)
   }
+
   const shareInfo = () => {
     navigator.share({
       title: `Dados do Covid19 - ${country}`,
       text: textCovid19,
-      url: 'covidiego.netlify.app/'
+      url: 'https://covid19dio.netlify.app/'
     })
   }
+
   const renderShareButton = (
     <div>
-      <Button variant ="contained" color="primary" onClick={shareInfo}>
+      <Button variant="contained" color="primary" onClick={shareInfo}>
         Compartilhar
       </Button>
     </div>
   )
+
   const renderCopyButton = (
     <div>
       <Button variant="contained" color="primary" onClick={copyInfo}>
@@ -40,12 +47,13 @@ function Panel({updateAt, onChange, data, country, getCoviddata}) {
       </Button>
     </div>
   )
+
   return (
     <Card>
       <CardPanelContentStyled>
         <div>
-          <Typography variant="h5" component="span" color="primary">COVID19 </Typography>
-          <Typography variant="h6" component="span" color="primary">Painel Coronavírus </Typography>
+          <Typography variant="h5" component="span" color="primary">COVID19</Typography>
+          <Typography variant="h6" component="span" color="primary">Painel Coronavírus</Typography>
           <Typography variant="body2" component="span" color="primary">Atualizado em: {updateAt}</Typography>
           <div className="pt-2">
             <Select onChange={onChange} value={country}>
@@ -53,9 +61,10 @@ function Panel({updateAt, onChange, data, country, getCoviddata}) {
             </Select>
           </div>
         </div>
-        {!navigatorHasShare ? renderShareButton : renderCopyButton}
+        {navigatorHasShare ? renderShareButton : renderCopyButton}
       </CardPanelContentStyled>
     </Card>
   )
 }
+
 export default memo(Panel)
